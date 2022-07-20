85435cab255f21c4e971e9b455a35dc33359a837
broMac <yhs4705@naver.com>
2022-07-20 20:05:16 +0900

    모야를 통한 통신코드 추가

--- MyMoyaExample/MoyaService.swift
+++ MyMoyaExample/MoyaService.swift
@@ -6,8 +6,44 @@
 //
 
 import Foundation
+import Moya
 
 
 enum MoyaService {
-    case tmdb
+    case searchTV(String)
+}
+
+extension MoyaService: TargetType {
+    var baseURL: URL {
+        return URL(string: URL.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .searchTV: return "/search/tv"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .searchTV: return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .searchTV(let query): return .requestParameters(parameters: [
+            "api_key": "\(APIKEY.apiKey)",
+            "query": "\(query)",
+            "language": "ko-KR",
+            "region": "ko-KR"
+        ], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-type": "application/json"]
+    }
+    
+    
 }
--- MyMoyaExample/MoyaViewController.swift
+++ MyMoyaExample/MoyaViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import Moya
+
 class MoyaViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
@@ -17,6 +19,27 @@ class MoyaViewController: UIViewController {
     
 
     @IBAction func buttonClicked(_ sender: UIButton) {
+        
+        let text = textField.text!
+        
+        let provider = MoyaProvider<MoyaService>()
+        provider.request(.searchTV(text)) { result in
+            switch result {
+            case let .success(moyaResponse):
+                let data = moyaResponse.data // Data, your JSON response is probably in here!
+                let statusCode = moyaResponse.statusCode
+                print(data)
+                print(statusCode)
+                do {
+                    let data = try JSONDecoder().decode(TvShows.self, from: data)
+                    print(data.results)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            case let .failure(error):
+                print(error)
+            }
+        }
         
     }
 
